eaa430e87c4f7fc0e67659307a255fda13bb28ea
Marcin Bieniek <marcingim@gmail.com>
2023-05-15 15:24:28 +0200

    Add lazy loading routes

--- vue-school-travel-app/src/router/index.js
+++ vue-school-travel-app/src/router/index.js
@@ -1,22 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import BrazilView from "../views/BrazilView.vue";
-import HawaiiView from "../views/HawaiiView.vue";
-import JamaicaView from "../views/JamaicaView.vue";
-import PanamaView from "../views/PanamaView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  { path: "/brazil", name: "brazil", component: BrazilView },
-  { path: "/hawaii", name: "hawaii", component: HawaiiView },
-  { path: "/jamaica", name: "jamaica", component: JamaicaView },
-  { path: "/panama", name: "panama", component: PanamaView },
+  { path: "/brazil", name: "brazil", component: () => import(/* webpackChunkName: "brazil" */ "../views/BrazilView.vue")},
+  { path: "/hawaii", name: "hawaii", component: () => import(/* webpackChunkName: "hawaii" */ "../views/HawaiiView.vue")},
+  { path: "/jamaica", name: "jamaica", component: () => import(/* webpackChunkName: "jamaica" */ "../views/JamaicaView.vue") },
+  { path: "/panama", name: "panama", component: () => import(/* webpackChunkName: "panama" */ "../views/PanamaView.vue") },
 ];
 
 const router = createRouter({
