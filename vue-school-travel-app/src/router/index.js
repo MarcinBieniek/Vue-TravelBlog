347b77eb37a8876aa72eec9bbfb699972c32dbd2
Marcin Bieniek <marcingim@gmail.com>
2023-05-16 23:05:54 +0200

    Generate routes based on props

--- vue-school-travel-app/src/router/index.js
+++ vue-school-travel-app/src/router/index.js
@@ -3,7 +3,11 @@ import HomeView from "../views/HomeView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
-  { path: "/destination/:id/:slug", name: "destination.show", component: () => import(/* webpackChunkName: "destination" */  "../views/DestinationShow.vue") },
+  { path: "/destination/:id/:slug",
+      name: "destination.show",
+      component: () => import(/* webpackChunkName: "destination" */  "../views/DestinationShow.vue"),
+      props: route=> ({id: parseInt(route.params.id)})
+  }
 ];
 
 const router = createRouter({
--- vue-school-travel-app/src/views/DestinationShow.vue
+++ vue-school-travel-app/src/views/DestinationShow.vue
@@ -12,26 +12,17 @@
 import sourceData from '../data.json'
 
 export default {
-  data(){
-    return {
-      destination: null
+  props: {
+    id: {
+      type: Number, required: true
     }
   },
   computed: {
-    destinationId(){
-      return parseInt(this.$route.params.id)
+    destination() {
+      return sourceData.destinations.find(
+        (destination) => destination.id === this.id
+      )
     }
   },
-  methods: {
-    async initData(){
-      const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`)
-      this.destination = await response.json()
-    }
-  },
-
-  async created(){
-    this.initData()
-    this.$watch(() => this.$route.params,this.initData)
-  }
 }
 </script>
