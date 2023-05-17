9367da05f3344571977a35f050ed50698b3ac819
Marcin Bieniek <marcingim@gmail.com>
2023-05-17 14:18:19 +0200

    Add Route Transitions effect

--- vue-school-travel-app/src/views/DestinationShow.vue
+++ vue-school-travel-app/src/views/DestinationShow.vue
@@ -1,35 +1,38 @@
 <template>
-  <section v-if="destination" class="destination">
-    <h1>{{ destination.name }}</h1>
-    <div class="destination-details">
-      <img :src="`/images/${destination.image}`" :alt="destination.name">
-      <p>{{destination.description}}</p>
-    </div>
-  </section>
-
-  <section class="experiences">
-    <h2>Top experiences in {{ destination.name }}</h2>
-    <div class="cards">
-      <router-link
-        v-for="experience in destination.experiences"
-        :key="experience.slug"
-        :to="{name: 'experience.show', params: {experienceSlug: experience.slug}}"
-      >
-        <ExperienceCard
-          :experience="experience"
-        />
-      </router-link>
-    </div>
-    <router-view />
-  </section>
+  <div>
+    <section v-if="destination" class="destination">
+      <h1>{{ destination.name }}</h1>
+      <GoBackButton />
+      <div class="destination-details">
+        <img :src="`/images/${destination.image}`" :alt="destination.name">
+        <p>{{destination.description}}</p>
+      </div>
+    </section>
+    <section class="experiences">
+      <h2>Top experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <router-link
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+          :to="{name: 'experience.show', params: {experienceSlug: experience.slug}}"
+        >
+          <ExperienceCard
+            :experience="experience"
+          />
+        </router-link>
+      </div>
+      <router-view />
+    </section>
+  </div>
 </template>
 
 <script>
 import sourceData from '../data.json'
 import ExperienceCard from '../components/ExperienceCard.vue'
+import GoBackButton from '../components/GoBackButton.vue'
 
 export default {
-  components: {ExperienceCard},
+  components: {ExperienceCard, GoBackButton},
   props: {
     id: {
       type: Number, required: true
